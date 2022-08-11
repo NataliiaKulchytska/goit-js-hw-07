73f43bf5e1de1669b735c7495abb26eea5b973aa
NataliiaKulchytska <kylchitskaia@gmail.com>
2022-08-11 18:56:20 +0300

    change on task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const containerGallery = document.querySelector('.gallery');
+const markupFunEl = markupDataArray(galleryItems)
+// console.log(divGallery);
+containerGallery.addEventListener('click', clickImgModal())
+
+function markupDataArray() {
+  return galleryItems.map(({ preview, original, description }) => {
+   return `<div class = gallery_item>
+         <a class = gallery__link href='${original}'>
+      <img
+      class= 'gallery__image'
+      src= '${preview}'
+      data-source= '${original}'
+      alt='${description}'
+    />
+  </a>
+</div>`})
+        .join('');
+    
+    
+};
+containerGallery.insertAdjacentHTML('afterbegin', markupFunEl);
+
+function clickImgModal(e) {
+  e.preventDefault();
+  
+};
