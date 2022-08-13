1c7b56af1599f4832517635ca5ac278274971e50
NataliiaKulchytska <kylchitskaia@gmail.com>
2022-08-13 12:19:59 +0300

    initial markup of tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,13 +4,19 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const containerGallery = document.querySelector('.gallery');
-const markupFunEl = markupDataArray(galleryItems)
+const image = document.querySelector('.gallery__image');
 // console.log(divGallery);
-containerGallery.addEventListener('click', clickImgModal())
+containerGallery.addEventListener('click', onClickImgModal);
+containerGallery.addEventListener('keydown', onCloseModalKey);
+ 
+// image.src = `${preview}`,
+//       image.alt = `${description}`,
+//       image.dataset.source = `${origin}`
 
-function markupDataArray() {
-  return galleryItems.map(({ preview, original, description }) => {
-   return `<div class = gallery_item>
+// const markupFunEl = markupDataArray;
+
+const markupDataArray = galleryItems.map(({ preview, original, description }) => 
+    `<div class = gallery_item>
          <a class = gallery__link href='${original}'>
       <img
       class= 'gallery__image'
@@ -19,14 +25,62 @@ function markupDataArray() {
       alt='${description}'
     />
   </a>
-</div>`})
+</div>`)
         .join('');
     
-    
-};
-containerGallery.insertAdjacentHTML('afterbegin', markupFunEl);
+   
+// function markupDataArray() {
 
-function clickImgModal(e) {
+
+//   return galleryItems.map(({ preview, original, description }) => {
+//    return `<div class = gallery_item>
+//          <a class = gallery__link href='${original}'>
+//       <img
+//       class= 'gallery__image'
+//       src= '${preview}'
+//       data-source= '${original}'
+//       alt='${description}'
+//     />
+//   </a>
+// </div>`})
+//         .join('');
+    
+   
+// };
+
+containerGallery.insertAdjacentHTML('afterbegin', markupDataArray);
+const instance = basicLightbox.create(`${markupDataArray}  width="800" height="600">`); 
+
+//  `<img src='${preview}'>`
+function onClickImgModal(e) {
   e.preventDefault();
   
+  const { target } = e;  
+  
+  if (target.localName === 'img') {
+//  image.dataset.source =`${origin}`
+     
+    
+    //    modalImgRef.src = e.target.dataset.source;
+    
+    // modalImgRef.alt = e.target.alt;
+    // modalImgRef.dataset.index = e.target.dataset.index;
+    instance.show();
+    
+    // console.log(instance);
+  } 
 };
+
+function onCloseModalKey(e) {
+  if (e.key === 'Escape') {
+      containerGallery.classList.remove('is-open');
+  }
+};
+// {
+//   onShow: (instance) => {},
+// 	/*
+// 	 * Function that gets executed before the lightbox closes.
+// 	 * Returning false will prevent the lightbox from closing.
+// 	 */
+// 	onClose: (instance) => {}
+// }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,48 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryList = document.querySelector('.gallery');
+const image = document.querySelector(".gallery__image");
+
+galleryList.addEventListener('click', onModalImg);
+
+//  _.debounce(onCaptionImg, 250)
+function galleryListMarkup() {
+    return galleryItems.map(({ preview, original, description }) => {
+ return `<li class = gallery_lists>
+         <a class = gallery__item href='${original}'>
+      <img
+      class= 'gallery__image'
+      src= '${preview}'      
+      alt='${description}'
+    />
+  </a>
+</li>`})
+        .join('');
+    
+};
+
+const markupFunEl = galleryListMarkup(galleryItems);
+galleryList.insertAdjacentHTML('afterbegin', markupFunEl);
+
+function onCaptionImg () {
+image.alt= `${description}`;
+}
+function onModalImg(e) {
+  e.preventDefault();  
+
+};
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+	// do something…
+  
+
+});
+gallery.on('error.simplelightbox', function (e) {
+	console.log(e); // some usefull information
+});
+
+
