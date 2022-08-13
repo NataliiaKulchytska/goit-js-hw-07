142ff52e37ece6299042fd2d5b4c583f9b5d153b
NataliiaKulchytska <kylchitskaia@gmail.com>
2022-08-13 20:31:29 +0300

    change 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,6 +9,7 @@ const image = document.querySelector(".gallery__image");
 
 galleryList.addEventListener('click', onModalImg);
 
+
 //  _.debounce(onCaptionImg, 250)
 function galleryListMarkup() {
     return galleryItems.map(({ preview, original, description }) => {
@@ -28,22 +29,36 @@ function galleryListMarkup() {
 const markupFunEl = galleryListMarkup(galleryItems);
 galleryList.insertAdjacentHTML('afterbegin', markupFunEl);
 
-function onCaptionImg () {
-image.alt= `${description}`;
-}
-function onModalImg(e) {
-  e.preventDefault();  
-
-};
-
-let gallery = new SimpleLightbox('.gallery a');
-gallery.on('show.simplelightbox', function () {
-	// do something…
+// galleryList.addEventListener('click',  _.debounce(() => {
+    
+// }, 250));
   
 
+function onModalImg(e) {
+  e.preventDefault();  
+};
+
+
+    let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function (e) { 
+  
+  
 });
 gallery.on('error.simplelightbox', function (e) {
 	console.log(e); // some usefull information
 });
 
+function onCaptionImg() {
+  // const { target } = e;
 
+  // if (target.localName === 'img') {
+  //   target = $('.gallery a').simpleLightbox({
+  //     optoins.captionType = `'${description}'`,
+  //     options.captionDelay= 250,
+  //     options.captionPosition= 'bottom'
+  //   });
+  //   console.log(onCaptionImg());
+  // }
+
+// if(options.captionType) handleScrollbar('hide');
+}
